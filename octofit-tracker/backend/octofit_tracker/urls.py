3d8aa5efbbace68d11e4bfe1781f59e90028eba6
sm-sl-build <shagiya.mansuri@slalom.com>
2025-11-06 06:26:15 +0000

    Refactor API root view to dynamically generate base URLs based on CODESPACE_NAME environment variable

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import os
 from rest_framework import routers
 from .views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
-CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
-BASE_API_PREFIX = 'api/'
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -33,16 +29,22 @@ router.register(r'workouts', WorkoutViewSet)
 
 @api_view(['GET'])
 def api_root(request):
+    import os
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    if codespace_name:
+        base_url = f'https://{codespace_name}-8000.app.github.dev/api/'
+    else:
+        base_url = 'http://localhost:8000/api/'
     return Response({
-        'users': '/users/',
-        'teams': '/teams/',
-        'activities': '/activities/',
-        'leaderboard': '/leaderboard/',
-        'workouts': '/workouts/',
+        'users': f'{base_url}users/',
+        'teams': f'{base_url}teams/',
+        'activities': f'{base_url}activities/',
+        'leaderboard': f'{base_url}leaderboard/',
+        'workouts': f'{base_url}workouts/',
     })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(f'{BASE_API_PREFIX}', include(router.urls)),
+    path('api/', include(router.urls)),
     path('', api_root, name='api_root'),
 ]
